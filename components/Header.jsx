f69a7f68bdf7bae749cb832cdef41c794c89740f
unknown <ahmedomar1007@gmail.com>
2021-11-23 17:21:23 +0200

    get categories and recent posts

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,19 @@
-import { useContext } from 'react'
-import Link from 'next/link'
+import React, { useState, useEffect } from 'react';
 
-const categories = [{name: 'React', slug: 'react'}]
+import Link from 'next/link';
+import { getCategories } from '../services';
+
 const Header = () => {
-    return (
-        <div className="container mx-auto px-10 mb-8">
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setCategories(newCategories);
+    });
+  }, []);
+
+  return (
+    <div className="container mx-auto px-10 mb-8">
       <div className="border-b w-full inline-block border-blue-400 py-8">
         <div className="md:float-left block">
           <Link href="/">
@@ -18,7 +27,7 @@ const Header = () => {
         </div>
       </div>
     </div>
-    )
-}
+  );
+};
 
-export default Header
+export default Header;
