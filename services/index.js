9110f0f9be79e81e4399c3e04b99618f46a472f0
Ahmed Abdelfattah <ahmedomar1007@gmail.com>
2021-12-21 07:08:49 +0200

    Change getRecentPosts  orderBy

--- services/index.js
+++ services/index.js
@@ -238,7 +238,7 @@ export const getRecentPosts = async () => {
   const query = gql`
     query GetPostDetails() {
       posts(
-        orderBy: createdAt_ASC
+        orderBy: createdAt_DSC
         last: 3
       ) {
         title
